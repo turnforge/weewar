887d2d514663672980fe0458d321e10e33b9f7a2
Sri Panyam <sri.panyam@gmail.com>
2025-09-07 23:26:21 -0700

    unused file

--- test_path_cli.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Test the path display in CLI
-cd cmd/weewar-cli
-
-# Try with the test game
-echo "Testing path display with game 32112070..."
-echo -e "options -1,-2\nquit" | ./weewar-cli 32112070
